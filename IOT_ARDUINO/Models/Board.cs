7871a94bf71d9216d9695cc3973368892d8bc743
Dragusin Sebastian <Sebastian-Alexandru.Dragusin@draexlmaier.com>
2022-12-30 18:43:30 +0200

    test2

---
+++ IOT_ARDUINO/Models/Board.cs
@@ -0,0 +1,73 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IOT_ARDUINO.Models
+{
+    public class Board
+    {
+        #region Fields
+
+        private string? id;
+        private string? deviceName;
+        private string? comPort;
+        private string? serialBaud;
+        private string? image;
+
+        #endregion
+
+        #region Constructors
+        #endregion
+
+        #region Proprietes
+
+        [Key]
+        public string? IdBoard
+        {
+            get => this.id;
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string? DeviceName
+        {
+            get => this.deviceName;
+            set
+            {
+                this.deviceName = value;
+            }
+        }
+
+        public string? ComPort
+        {
+            get => this.comPort;
+            set
+            {
+                this.comPort = value;
+            }
+        }
+
+        public string? SerialBaud
+        {
+            get => this.serialBaud;
+            set
+            {
+                this.serialBaud = value;
+            }
+        }
+
+        public string? Image
+        {
+            get => this.image;
+            set
+            {
+                this.image = value;
+            }
+        }
+
+        #endregion
+
+        #region Methods
+        #endregion
+    }
+}
---
+++ IOT_ARDUINO/Models/Data.cs
@@ -0,0 +1,67 @@
+﻿namespace IOT_ARDUINO.Models
+{
+    public class Data
+    {
+        #region Fields
+
+        private string? batteryPercent;
+        private string? value;
+        private SensorType type;
+        private string? image;
+
+        #endregion
+
+        #region Constructors
+        #endregion
+
+        #region Proprietes
+
+        public string? BatteryPercent
+        {
+            get => this.batteryPercent;
+            set
+            {
+                batteryPercent = value;
+            }
+        }
+
+        public string? Value
+        {
+            get => this.value;
+            set
+            {
+                this.value = value;
+            }
+        }
+
+        public SensorType Type
+        {
+            get => this.type;
+            set
+            {
+                this.type = value;
+            }
+        }
+
+        public string? Image
+        {
+            get => this.image;
+            set
+            {
+                this.image = value;
+            }
+        }
+
+        #endregion
+
+        #region Methods
+        #endregion
+    }
+
+    public enum SensorType
+    {
+        Temperature,
+        Humidity,
+        Gas
+    }
+}
